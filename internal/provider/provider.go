d591ec1aeb14cc15f13f7ef98d2c8448f5be10be
Sachin Sharma <sharmasachin1309@gmail.com>
2024-06-03 00:57:28 +0530

    provider_config

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,17 +2,26 @@ package provider
 
 import (
 	"context"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
 	_ provider.Provider = &todoProvider{}
 )
+
+// todoProviderModel maps provider schema data to a Go type.
+type todoProviderModel struct {
+	Host  types.String `tfsdk:"host"`
+	Token types.String `tfsdk:"token"`
+}
 
 // New is a helper function to simplify provider server and testing implementation.
 func New(version string) func() provider.Provider {
@@ -39,11 +48,106 @@ func (p *todoProvider) Metadata(_ context.Context, _ provider.MetadataRequest, r
 
 // Schema defines the provider-level schema for configuration data.
 func (p *todoProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"host": schema.StringAttribute{
+				Optional: true,
+			},
+			"token": schema.StringAttribute{
+				Optional:  true,
+				Sensitive: true,
+			},
+		},
+	}
 }
 
 // Configure prepares a HashiCups API client for data sources and resources.
 func (p *todoProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	// Retrieve provider data from configuration
+	var config todoProviderModel
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If practitioner provided a configuration value for any of the
+	// attributes, it must be a known value.
+
+	if config.Host.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("host"),
+			"Unknown TODO API Host",
+			"The provider cannot create the TODO API client as there is an unknown configuration value for the HashiCups API host. ",
+		)
+	}
+
+	if config.Token.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("token"),
+			"Unknown TODO API token",
+			"The provider cannot create the TODO API client as there is an unknown configuration value for the TODO API token. ",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Default values to environment variables, but override
+	// with Terraform configuration value if set.
+
+	host := os.Getenv("TODO_HOST")
+	token := os.Getenv("TODO_TOKEN")
+
+	if !config.Host.IsNull() {
+		host = config.Host.ValueString()
+	}
+
+	if !config.Token.IsNull() {
+		token = config.Token.ValueString()
+	}
+
+	// If any of the expected configurations are missing, return
+	// errors with provider-specific guidance.
+
+	if host == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("host"),
+			"Missing TODO API Host",
+			"The provider cannot create the TODO API client as there is a missing or empty value for the TODO API host. ",
+		)
+	}
+
+	if token == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("token"),
+			"Missing TODO API token",
+			"The provider cannot create the TODO API client as there is a missing or empty value for the TODO API token. ",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Create a new TODO client using the configuration values
+	// TODO: fix the client 
+	client, err := todo.NewClient(&host, &token)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Create TODO API Client",
+			"An unexpected error occurred when creating the TODO API client. "+
+				"If the error is not clear, please contact the provider developers.\n\n"+
+				"TODO Client Error: "+err.Error(),
+		)
+		return
+	}
+
+	// Make the TODO client available during DataSource and Resource
+	// type Configure methods.
+	resp.DataSourceData = client
+	resp.ResourceData = client
 }
 
 // DataSources defines the data sources implemented in the provider.
